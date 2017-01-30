e32505dcd6303fd5fba2537cff3966c676194e84
Михаил <m.smirnov@cuberto.ru>
2017-01-30 16:50:23 +0300

    Add out animation

--- src/javascript/states/main.js
+++ src/javascript/states/main.js
@@ -1,12 +1,17 @@
 angular.module('app')
-	.controller('main', ['$scope', '$state', main]);
+	.controller('main', ['$timeout', '$state', '$rootScope', main]);
 
-function main($scope, $state) {
+function main($timeout, $state, $rootScope) {
 	var vm = this;
 	
-	vm.goToState = function(state){
-		$state.go(state);
-		// console.log(state);
-	};
+	vm.isAnimating = false;
 	
+	vm.goToState = function(state){
+		vm.isAnimating = true;
+		
+		$timeout(function() {
+			$state.go(state);
+			vm.isAnimating = false;
+		}, 2000);
+	};
 }
